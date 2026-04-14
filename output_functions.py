8b5029ca8b728666bff9abe89e5c9aead6d50862
localuser <user@local>
2026-04-13 22:40:12 -0400

    modified output_functions.py generating damage per skill by player to generate skill dmg breakdown for all players with damage against targets without restricting by critical rate frequency to allow Shatter Aegis FB to show in log

--- output_functions.py
+++ output_functions.py
@@ -3220,7 +3220,7 @@ def build_damage_outgoing_by_player_skill_tids(top_stats: dict, skill_data: dict
 		player: data['dpsTargets']['damage']
 		for player, data in top_stats['player'].items()
 		if data['statsTargets']['critableDirectDamageCount'] > 0
-		and (data['statsTargets']['criticalRate'] / data['statsTargets']['critableDirectDamageCount']) > 0.45
+		#and (data['statsTargets']['criticalRate'] / data['statsTargets']['critableDirectDamageCount']) > 0.45
 		and data['dpsTargets']['damage'] > 0
 	}	
 
